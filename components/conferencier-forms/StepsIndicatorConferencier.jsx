7c61366069c1950bfee69b4f849888340aecb66a
HhDreamer <hh.dreamer@gmail.com>
2023-07-21 15:51:43 +0200

    commit remove dev change page.

--- components/conferencier-forms/StepsIndicatorConferencier.jsx
+++ components/conferencier-forms/StepsIndicatorConferencier.jsx
@@ -11,9 +11,9 @@ const StepsIndicatorConferencier = ({ currentStep }) => {
   // ];
   const stepsName = ["", "", "", "", ""];
 
-  const handleStepClick = (step) => {
-    setStep(step);
-  };
+  // const handleStepClick = (step) => {
+  //   setStep(step);
+  // };
 
   return (
     <div className='flex justify-center space-x-8 mt-10'>
@@ -21,7 +21,7 @@ const StepsIndicatorConferencier = ({ currentStep }) => {
         <div
           key={index}
           className='flex flex-col items-center'
-          onClick={() => handleStepClick(index + 1)}
+          // onClick={() => handleStepClick(index + 1)}
         >
           <div
             className={`w-8 h-8 rounded-full border border-slate-950 flex items-center justify-center ${
